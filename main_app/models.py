d6d1814c4ba25b2bafe900849f8457474880867a
Your Name <you@example.com>
2019-03-11 14:50:35 -0700

    create car details route, view and template

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    color = models.CharField(max_length=100)
+    engine = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
+    year = models.IntegerField()
+    def __str__(self):
+        return f'{self.year} {self.make} {self.style}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Car():
-    def __init__(self, color, engine, make, model, driver, year):
-        self.color = color
-        self.engine = engine
-        self.make = make
-        self.model = model
-        self.driver = driver
-        self.year = year
-
-cars = [
-    Car('red', 'V6', 'Honda', 'Civic', 'Sam', 2004),
-    Car('yellow', 'V8', 'Toyota', 'Carolla', 'John', 2012),
-    Car('blue', 'hamster running on a stationary wheel', 'Ford', 'Taurus', 'Sally', 2020),
-]
+from .models import Car
 
 # Create your views here.
 
@@ -25,4 +10,9 @@ def about(request):
     return render(request, 'about.html') 
 
 def cars_index(request):
+    cars = Car.objects.order_by('-year')
     return render(request, 'cars/index.html', {'cars': cars})
+
+def car_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {'car': car})
